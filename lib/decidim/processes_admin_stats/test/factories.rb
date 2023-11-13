3c8b517131210abc06aa88e8ec616958a67fca0b
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2023-11-13 17:24:25 +0100

    Fix factories

--- lib/decidim/processes_admin_stats/test/factories.rb
+++ lib/decidim/processes_admin_stats/test/factories.rb
@@ -6,7 +6,7 @@ require "decidim/proposals/test/factories"
 FactoryBot.define do
   factory :processes_admin_stats_component, parent: :component do
     name { Decidim::Components::Namer.new(participatory_space.organization.available_locales, :processes_admin_stats).i18n_name }
-    manifest_name :processes_admin_stats
+    manifest_name { :processes_admin_stats }
     participatory_space { create(:participatory_process, :with_steps) }
   end
 
